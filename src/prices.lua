2877cee1d786ec432c883d21d47fc3d8a2a35e28
R0ssinante <jollys.wollys@gmail.com>
2020-07-17 19:49:03 +0300

    Сруктура и инициализация бд для продукта, магазина и штрих-кода

--- src/prices.lua
+++ src/prices.lua
@@ -4,17 +4,102 @@ local log = require("log")
 
 local app_name = 'prices'
 
+local function init_space()
+
+    local if_not_exists = true
+
+    local prices = box.schema.space.create(
+        'prices',
+        {
+            -- формат хранимых кортежей
+            format = {
+                {'id', 'unsigned'},
+                {'price_value', 'double'},
+                {'date_created','string'},
+                {'approved', 'boolean'},
+                {'product_id','unsigned'},
+                {'shop_id','unsigned'},
+            },
+
+            if_not_exists = if_not_exists,
+        }
+    )
+
+    prices:create_index('primary', {
+        type = "hash",
+        parts = {'id'},
+        if_not_exists = if_not_exists,
+    })
+
+    local products = box.schema.space.create(
+        'products',
+        {
+            -- формат хранимых кортежей
+            format = {
+                {'id', 'unsigned'},
+                {'name','string'},
+                {'uuid','string'},
+            },
+
+            if_not_exists = if_not_exists,
+        }
+    )
+
+    products:create_index('primary', {
+        type = "hash",
+        parts = {'id'},
+        if_not_exists = if_not_exists,
+    })
+
+    local shops = box.schema.space.create(
+        'shops',
+        {
+            -- формат хранимых кортежей
+            format = {
+                {'id', 'unsigned'},
+                {'name','string'},
+                {'uuid','string'},
+            },
+
+            if_not_exists = if_not_exists,
+        }
+    )
+
+    shops:create_index('primary', {
+        type = "hash",
+        parts = {'id'},
+        if_not_exists = if_not_exists,
+    })
+
+    local barcodes = box.schema.space.create(
+        'barcodes',
+        {
+            -- формат хранимых кортежей
+            format = {
+                {'product_id', 'unsigned'},
+                {'barcode','string'},
+            },
+
+            if_not_exists = if_not_exists,
+        }
+    )
+
+    barcodes:create_index('primary', {
+        type = "hash",
+        parts = {'product_id'},
+        if_not_exists = if_not_exists,
+    })
+
+
+end
+
 local app = {
     
     price_model = {},
 
     start = function (self)
-        box.once('init', function ()
-            box.schema.create_space("prices")
-            box.space.prices:create_index(
-                "primary", {type = "hash", parts = {1, "unsigned"}}
-            )
-        end)
+
+        init_space()
 
         local ok_p, price = avro.create(schema.price)
 
@@ -35,6 +120,8 @@ local app = {
         return false
 
     end
+
+
 }
 
 return app
