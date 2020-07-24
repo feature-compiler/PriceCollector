ae3069d210a1d9d1a12a7cba1499e3143c45b332
R0ssinante <jollys.wollys@gmail.com>
2020-07-24 19:07:15 +0300

    Проверка цифр телефона

--- src/smsc.lua
+++ src/smsc.lua
@@ -8,6 +8,11 @@ local phone = "89107138699"
 local message = "test message"
 local sender = "ZELENOSTROV"
 
+local function check_number(phone)
+    if string.find(phone,"8") ~= 1 then
+        error("Phone should be starts with '8' !!!")
+    end
+end
 
 local function request(URL)
     local response = http.get(URL)
@@ -15,6 +20,7 @@ local function request(URL)
 end
 
 local function send_message(phone, message)
+    check_number(phone)
     local url = string.format("https://smsc.ru/sys/send.php?login=%s&psw=%s&phones=%s&mes=%s&sender=%s&fmt=3", login, password, phone, string.gsub(message, "%s+", "_"), sender)
     local response = request(url)
     return response
